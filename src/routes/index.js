19e9db11a05547a02d024df2db75ac045fb01c26
SHIN1501020 <shin8hh@gmail.com>
2023-09-07 13:43:38 +0900

    routes/index.js router 경로 설정

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import UsersRouter from "./users.js";
+import PostsRouter from "./posts.js";
+import CommentsRouter from "./comments.js";
+
+const router = express.Router();
+
+router.use("/", UsersRouter);
+//router.use("/posts", [PostsRouter, CommentsRouter]);
+
+export default router;
